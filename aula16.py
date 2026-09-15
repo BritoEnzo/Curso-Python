7f77843cb15e4186d3e06a0575f7972964bb07ea
Enzo Rafael Oliveira Brito <enzorafael82@gmail.com>
2026-09-15 19:48:06 -0300

    Add files via upload

---
+++ aula16.py
@@ -0,0 +1,11 @@
+entrada = input('Digite "entrar" para entrar ou "sair" para sair : ')
+
+if entrada == 'entrar':
+    print ('Seja bem vindo')
+elif entrada == 'sair':
+    print ('Tchau tchau')
+elif entrada == '':
+    print ('digite algo válido')
+else:
+    print ('escolha uma das opções : "entrar" ou "sair"')
+    
---
+++ aula4.py
@@ -0,0 +1,8 @@
+idade = 18
+luiz = 15
+maioridade = idade >= 18
+
+if luiz >= 18 :
+    print("Luiz tem", luiz, "anos de idade, por isso ele é maior")
+else:
+    print("Luiz tem", luiz, "anos de idade, por isso ele é menor")
